03a264b9ce267c063594d33383862e723488e7ad
luiangvia <luiangvia@gmail.com>
2019-05-15 16:49:56 -0700

    works?

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
-var db = require("./db/models");
+// var db = require("./db/models");
 const app = express();
 const bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3001;
 
-const routes = require("./routes");
+// const routes = require("./routes");
 
 // app.use(bodyParser.urlencoded({
 //   extended: true
@@ -20,14 +20,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(routes);
+// app.use(routes);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-db.sequelize.sync().then(function () {
+// db.sequelize.sync().then(function () {
   app.listen(PORT, () => {
     console.log(`App listening on PORT" + ${PORT}!`);
   });
-});
+// });

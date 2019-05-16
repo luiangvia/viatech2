29755d056fdf872b13ac223ddd493d9ab7b669a2
luiangvia <luiangvia@gmail.com>
2019-05-15 20:16:22 -0700

    adding if-env

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const path = require("path");
-// var db = require("./db/models");
+var db = require("./db/models");
 const app = express();
 const bodyParser = require("body-parser");
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
+const mysql_import = require('mysql-import');
 
-// const routes = require("./routes");
+const routes = require("./routes");
 
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,14 +21,28 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// app.use(routes);
+app.use(routes);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// db.sequelize.sync().then(function () {
+db.sequelize.sync().then(function () {
   app.listen(PORT, () => {
     console.log(`App listening on PORT" + ${PORT}!`);
   });
-// });
+});
+
+setTimeout(seedDB, 10000);
+
+function seedDB(){
+require('mysql-import').config({
+	host: '127.0.0.1',
+	user: 'root',
+	password: 'sss',
+	database: 'viatech',
+	onerror: err=>console.log(err.message)
+}).import('seed.sql').then(()=> {
+	console.log('all sql statements have been executed')
+});
+}

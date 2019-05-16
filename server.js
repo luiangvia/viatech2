047fa6e099f938e884cff792f3cd47c9823a7389
luiangvia <luiangvia@gmail.com>
2019-05-15 17:30:52 -0700

    pleasework

--- server.js
+++ server.js
@@ -1,58 +1,33 @@
-// const express = require("express");
-// const path = require("path");
-// // var db = require("./db/models");
-// const app = express();
-// const bodyParser = require("body-parser");
-// var PORT = process.env.PORT || 3001;
-
-// // const routes = require("./routes");
-
-// // app.use(bodyParser.urlencoded({
-// //   extended: true
-// // }));
-// // app.use(bodyParser.json());
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
-// // app.use(routes);
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-// // db.sequelize.sync().then(function () {
-//   app.listen(PORT, () => {
-//     console.log(`App listening on PORT" + ${PORT}!`);
-//   });
-// // });
-
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+// var db = require("./db/models");
 const app = express();
+const bodyParser = require("body-parser");
+var PORT = process.env.PORT || 3001;
 
-// Define middleware here
+// const routes = require("./routes");
+
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+// app.use(bodyParser.json());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+// db.sequelize.sync().then(function () {
+  app.listen(PORT, () => {
+    console.log(`App listening on PORT" + ${PORT}!`);
+  });
+// });
